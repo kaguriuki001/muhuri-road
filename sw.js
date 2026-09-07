12a79d3c48267b679f33ad73e6f4dcb0267d96b6
unknown <Administrator@DANIEL-MKI.ksg.ac.ke>
2026-09-07 10:46:09 +0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "muhuri-road-v1";
+const URLS_TO_CACHE = ["/"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
